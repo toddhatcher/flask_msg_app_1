96a0fbe51b9f9293a0e0577f695c17697189f6d6
Todd Hatcher <toddchatcher@hotmail.com>
2024-06-05 22:42:54 -0500

    First Commit of Flask Msg App.

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template, request, url_for, flash, redirect
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '3be450637872ed576a48ab0e85d1ce1b1cf09018dcefdac7'
+
+messages = [{'title': 'Message One',
+             'content': 'Message One Content'},
+            {'title': 'Message Two',
+             'content': 'Message Two Content'}
+            ]
+
+@app.route('/')
+def index():
+    return render_template('index.html', messages=messages)
+
+@app.route('/create/', methods=('GET', 'POST'))
+def create():
+    if request.method == 'POST':
+        title = request.form['title']
+        content = request.form['content']
+
+        if not title:
+            flash('Title is required!')
+        elif not content:
+            flash('Content is required!')
+        else:
+            messages.append({'title': title, 'content': content})
+            return redirect(url_for('index'))
+
+    return render_template('create.html')
